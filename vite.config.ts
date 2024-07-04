c328066b724b7fb74fdb3ac415dd9e22afbab10e
lennsa <lennsa999@gmx.de>
2024-07-04 20:57:48 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), viteSingleFile()],
+  base: '/resistor-tool/',
+  // plugins: [svelte(), viteSingleFile()],
+  plugins: [svelte()],
 })
